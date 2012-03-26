1d35b378a2992327f5cc6667460f197e5079bb62
Chris Bell <themk1boy@yahoo.co.uk>
2012-03-26 20:09:35 +0100

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PusherCollage::Application.config.secret_token = '6c878102b72b2daaf0dfdfec9e15670154606a959b31c155cd4e4a5dda942a2f0d97f8d7d01df43f638fcb06aa5c645b7428f328a129a314d6633029b3834569'
